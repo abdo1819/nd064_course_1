409e775bf5b854c6834ac0e2820c928474e77f16
abdo hashem <ar1813@fayoum.edu.eg>
2021-07-01 22:58:04 +0200

    impl: L2EX13 add loginig for endpoints

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,17 +1,32 @@
-from flask import Flask
+from flask import Flask,request
+from logging import basicConfig,DEBUG
+import time
+
+
+basicConfig(filename="app.log",level=DEBUG)
+
 app = Flask(__name__)
+
+def log_endpoint(request: request):
+    app.logger.info(f"{time.asctime()}, {request.path} endpoint was reached")
+    
 
 @app.route("/")
 def hello():
+    log_endpoint(request)
     return "Hello World!"
 
 @app.route("/status")
 def status():
+    log_endpoint(request)
     return {"result": "OK - healthy"}
 
 @app.route("/metrics")
 def metrics():
+    log_endpoint(request)
     return {"data": {"UserCount": 140, "UserCountActive": 23}}
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
